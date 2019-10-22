279172a7150fde62bf1d920e4a4bbe1267b02df2
Dannar <dmawardi@gmail.com>
2019-10-22 15:45:31 +1100

    Updates

---
+++ precache-manifest.17cacd7c69997d4d8355e3474a948f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2cc186e198397824fb29489cfae6da",
+    "url": "/react-clicky-app/index.html"
+  },
+  {
+    "revision": "fba37755f6f6d7658524",
+    "url": "/react-clicky-app/static/css/main.77ebc2fc.chunk.css"
+  },
+  {
+    "revision": "99639354aba431c5a1a0",
+    "url": "/react-clicky-app/static/js/2.74003a14.chunk.js"
+  },
+  {
+    "revision": "fba37755f6f6d7658524",
+    "url": "/react-clicky-app/static/js/main.8661c48f.chunk.js"
+  },
+  {
+    "revision": "53d8a9d514be0826bdcf",
+    "url": "/react-clicky-app/static/js/runtime-main.dc29d9c6.js"
+  },
+  {
+    "revision": "15781a1ee7eb1f729c5fbcbe239cc63b",
+    "url": "/react-clicky-app/static/media/Final-Fantasy-Background.15781a1e.jpg"
+  }
+]);
